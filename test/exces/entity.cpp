89deb7b0142cb471e09ab6c70858fbdfabe441d1
Matus Chochlik <chochlik@gmail.com>
2013-05-28 14:21:40 +0200

    Update of the test suite

--- test/exces/entity.cpp
+++ test/exces/entity.cpp
@@ -9,10 +9,12 @@
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE EXCES_Angle
+#define BOOST_TEST_MODULE EXCES_Entity
 #include <boost/test/unit_test.hpp>
 
 #include <exces/entity.hpp>
+
+#include <vector>
 
 BOOST_AUTO_TEST_SUITE(Entity)
 
@@ -20,6 +22,40 @@ BOOST_AUTO_TEST_CASE(Entity_default_construction)
 {
 	exces::entity<>::type e;
 	(void)(e);
+}
+
+BOOST_AUTO_TEST_CASE(Entity_copy_construction)
+{
+	exces::entity<>::type e1;
+	exces::entity<>::type e2 = e1;
+	(void)(e2);
+}
+
+BOOST_AUTO_TEST_CASE(Entity_identity)
+{
+	exces::entity<>::type e1, e2;
+	BOOST_CHECK(e1 != e2);
+	exces::entity<>::type e1_c = e1;
+	BOOST_CHECK(e1 == e1_c);
+}
+
+
+BOOST_AUTO_TEST_CASE(Entity_identity_2)
+{
+	std::vector<exces::entity<>::type> ev(1000);
+	auto b = ev.begin(), e = ev.end();
+	auto i = b;
+	while(i != e)
+	{
+		auto j = i;
+		++j;
+		while(j != e)
+		{
+			BOOST_CHECK(*i != *j);
+			++j;
+		}
+		++i;
+	}
 }
 
 BOOST_AUTO_TEST_SUITE_END()
