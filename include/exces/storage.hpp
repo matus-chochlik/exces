9162742968d04bcd7cf71c1ac8b88e70dbba79b9
Matus Chochlik <chochlik@gmail.com>
2013-06-30 20:24:07 +0200

    Documentation update

--- include/exces/storage.hpp
+++ include/exces/storage.hpp
@@ -234,49 +234,58 @@ private:
 		return mp::get<component_id<Component, Group>::value>(_store);
 	}
 public:
+	/// The component key type
 	typedef std::size_t component_key;
 
+	/// Return a special NULL component key value
 	static component_key null_key(void)
 	{
 		return component_key(-1);
 	}
 
+	/// Reserves space for n instances of Component
 	template <typename Component>
 	void reserve(std::size_t n)
 	{
 		_store_of<Component>().reserve(n);
 	}
 
+	/// Access the specified Component type by its key
 	template <typename Component>
 	Component& access(component_key key)
 	{
 		return _store_of<Component>().at(key)._component;
 	}
 
+	/// Stores the specified component and returns
 	template <typename Component>
 	component_key store(Component&& component)
 	{
 		return _store_of<Component>().store(std::move(component));
 	}
 
+	/// Replaces the value of Component at the specified key
 	template <typename Component>
 	component_key replace(component_key key, Component&& component)
 	{
 		return _store_of<Component>().replace(key,std::move(component));
 	}
 
+	/// Copies the component at the specified key returns the new key
 	template <typename Component>
 	component_key copy(component_key key)
 	{
 		return _store_of<Component>.copy(key);
 	}
 
+	/// Adds reference to the component at the specified key
 	template <typename Component>
 	void add_ref(component_key key)
 	{
 		_store_of<Component>().add_ref(key);
 	}
 
+	/// Releases (removes reference to) the component at the specified key
 	template <typename Component>
 	bool release(component_key key)
 	{
