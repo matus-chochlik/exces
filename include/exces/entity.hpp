1f54b000496dee1af87be160429e5a95c59684d1
Matus Chochlik <chochlik@gmail.com>
2014-07-10 08:16:53 +0200

    Added a macro for easy entity type selection

--- include/exces/entity.hpp
+++ include/exces/entity.hpp
@@ -2,7 +2,7 @@
  *  @file exces/entity.hpp
  *  @brief Implements entity
  *
- *  Copyright 2012-2013 Matus Chochlik. Distributed under the Boost
+ *  Copyright 2012-2014 Matus Chochlik. Distributed under the Boost
  *  Software License, Version 1.0. (See accompanying file
  *  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  */
@@ -12,10 +12,6 @@
 
 #include <exces/group.hpp>
 #include <exces/entity/default.hpp>
-
-#if EXCES_BOOST_UUID_FOUND
-#include <exces/entity/boost_uuid.hpp>
-#endif
 
 namespace exces {
 
@@ -27,6 +23,15 @@ struct entity
 };
 
 } // namespace exces
+
+#define EXCES_USE_ENTITY_TYPE(GROUP, ENTITY_TYPE) \
+namespace exces { \
+template <> \
+struct entity<EXCES_GROUP_SEL(GROUP)> \
+{ \
+	typedef ENTITY_TYPE##_entity type; \
+};\
+}
 
 #endif //include guard
 
